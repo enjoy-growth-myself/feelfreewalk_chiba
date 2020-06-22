a6704f0e7ea1d9578ec9f02209ab297c7e3b08df
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-06-23 00:49:51 +0900

    add_user-controllor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
 	def index
+		@users = User.all
+		@user = current_user
 	end
 
 	def show
+    	params_user = User.find(params[:id])
+	    if current_user == params_user
+	       @user = User.find(current_user.id)
+	    else
+	       @user = User.find(params[:id])
+	    end
 	end
 
 	def create
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true,length: {maximum: 20, minimum: 2}
+  
+  attachment :profile_image, destroy: false
 end
