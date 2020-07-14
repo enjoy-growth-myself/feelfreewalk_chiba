41c38b2591f274fce722f2d55d0ae497346ae9ae
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-07-15 01:42:04 +0900

    add_postarticle-show-preview

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -109,3 +109,25 @@ $(document).ready(function(){
   });
 });
 
+
+$(document).ready(function(){
+  $('#postarticle_image_image').change(function(e){
+    //ファイルオブジェクトを取得する
+    let file = e.target.files[0];
+    let reader = new FileReader();
+    //画像でない場合は処理終了
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください。");
+      return false;
+    }
+    //アップロードした画像を設定する
+    reader.onload = (function(file){
+      return function(e){
+        $("#img").attr("src", e.target.result);
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
+
+
