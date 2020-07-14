470c1cbc5112c3b8adb51db4409691c2cbdffb90
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-07-14 18:39:20 +0900

    modified_layout-devise-session-notice add_preview-user-show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -82,11 +82,30 @@ $(document).ready(function(){
     //アップロードした画像を設定する
     reader.onload = (function(file){
       return function(e){
-        $("#img1").attr("src", e.target.result);
+        $("#img").attr("src", e.target.result);
       };
     })(file);
     reader.readAsDataURL(file);
   });
 });
 
+$(document).ready(function(){
+  $('#user_profile_image').change(function(e){
+    //ファイルオブジェクトを取得する
+    let file = e.target.files[0];
+    let reader = new FileReader();
+    //画像でない場合は処理終了
+    if(file.type.indexOf("image") < 0){
+      alert("画像ファイルを指定してください。");
+      return false;
+    }
+    //アップロードした画像を設定する
+    reader.onload = (function(file){
+      return function(e){
+        $("#img").attr("src", e.target.result);
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
 
