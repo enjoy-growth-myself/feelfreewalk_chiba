0c91eb7a0281414eb52b51c9e35d022c7c3e957a
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-08-12 01:38:59 +0900

    modified models/admin.rb, models/user.rb add devise :database_authenticatable, authentication_keys: becouse login of user is name and login admin is email

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,8 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  devise :database_authenticatable, authentication_keys: [:email]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
