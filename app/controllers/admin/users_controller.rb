8e850ccfa10fff8002714577784df1fef7cd9606
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-08-12 21:32:50 +0900

    add controller,view admin_user_show

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,9 @@ class Admin::UsersController < ApplicationController
 	def index
 		@users = User.all.page(params[:page]).per(10).reverse_order
 	end
+
+	def show
+		@user = User.find(params[:id])
+		@postarticles = @user.postarticles.page(params[:page]).per(10).reverse_order
+	end
 end
