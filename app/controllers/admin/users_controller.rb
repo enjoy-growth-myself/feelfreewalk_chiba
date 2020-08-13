9789911154e7d3dcb4b5c134cf66ace8a14ec7a3
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-08-13 14:30:10 +0900

    add modle user/is_valid add controller/admin/users/update add users_is_valid_update to admin_users_view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,4 +7,19 @@ class Admin::UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@postarticles = @user.postarticles.page(params[:page]).per(10).reverse_order
 	end
+
+	def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user)
+      flash[:notice] = "更新されました!"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:is_valid)
+  end
 end
