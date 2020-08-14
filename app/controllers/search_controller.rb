cb4c27a4f85aef07ccafb430c86b5623ad5cdaaa
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-08-14 13:07:05 +0900

    add user/search/controller changed from if to ternary operator

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,14 @@
 class SearchController < ApplicationController
+	# def search
+	# 	if params[:keyword] == "title"
+	# 		@postarticles = Postarticle.where('title LIKE ?', "%#{params[:searchword]}%").page(params[:page]).per(10).reverse_order
+	# 	else
+	# 		@postarticles = Postarticle.where('address LIKE ?', "%#{params[:searchword]}%").page(params[:page]).per(10).reverse_order
+	# 	end
+	# end
+
 	def search
-		if params[:keyword] == "title"
-			@postarticles = Postarticle.where('title LIKE ?', "%#{params[:searchword]}%").page(params[:page]).per(10).reverse_order
-		else
-			@postarticles = Postarticle.where('address LIKE ?', "%#{params[:searchword]}%").page(params[:page]).per(10).reverse_order
-		end
+		search_colums = params[:keyword] == "title"? 'title LIKE ?':'address LIKE ?'
+		@postarticles = Postarticle.where(search_colums, "%#{params[:searchword]}%").page(params[:page]).per(10).reverse_order
 	end
 end
